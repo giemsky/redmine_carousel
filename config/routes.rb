34342c50463fd3d5f7447d98f37f257362fd6db5
Grzegorz Miklaszewski <mail@gmiklaszewski.pl>
2010-11-28 21:44:01 +0100

    Fixed error while saving project settings after installation of redmine_carousel.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  
-  map.resources :projects do |p|
-  	p.resources :carousels, :except => [:index, :show]
-  end
-  
+	map.resources :carousels, :except => [:index, :show], :path_prefix => 'projects/:project_id', :name_prefix => 'project_'
 end
